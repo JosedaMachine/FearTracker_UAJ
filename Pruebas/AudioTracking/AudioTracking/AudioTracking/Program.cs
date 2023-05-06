81e0c78093a077dd77d621b3510411846eb2b09b
Ángel López <62568478+angelo066@users.noreply.github.com>
2023-05-06 10:39:15 +0200

    Ya no es todo estático

--- Pruebas/AudioTracking/AudioTracking/AudioTracking/Program.cs
+++ Pruebas/AudioTracking/AudioTracking/AudioTracking/Program.cs
@@ -1,47 +1,46 @@
 ﻿using NAudio.CoreAudioApi;
+using System.Reflection.Metadata.Ecma335;
 
 namespace AudioTracking
 {
-    internal class Program
+    class AudioTracking
     {
-        static MMDeviceEnumerator en; //La variable que lleva los devices
+        MMDeviceEnumerator en; //La variable que lleva los devices
 
         //Todos estos numeritos estaría bien poder tocarlos de alguna manera
 
 
         //Quizás a esto haya que ponerle estático, no lo se
-        static float defaultspakingVolume = 4000.0f;   //Basado en las pruebas que hemos hecho, esto da más o menos si hablas a un tono normal (Por si mi colegui no quiere hacer los test)
-        static float voiceMult = 100000.0f; //Para que el volumen sean números mayores que cero
+        float defaultspakingVolume = 4000.0f;   //Basado en las pruebas que hemos hecho, esto da más o menos si hablas a un tono normal (Por si mi colegui no quiere hacer los test)
+        float voiceMult = 100000.0f; //Para que el volumen sean números mayores que cero
 
-        static int voiceTests = 3; //El número de veces que tiene que hablar el usuario
+        int voiceTests = 3; //El número de veces que tiene que hablar el usuario
 
-        static float backgroundNoise = 15.0f; //Basado en el ruido monstruoso que hace mi pc
+        float backgroundNoise = 15.0f; //Basado en el ruido monstruoso que hace mi pc
 
 
-        static int backgroundTimer = 50;    //Iteraciones que nos pasamos grabando, si queremos ponerlo como tiempo pues hay que ver cuantas iteraciones hace por segundo
+        int backgroundTimer = 50;    //Iteraciones que nos pasamos grabando, si queremos ponerlo como tiempo pues hay que ver cuantas iteraciones hace por segundo
 
-        static void initvoiceDetection()
-        {
-            en = new MMDeviceEnumerator();
-        }
 
         //Hay que seleccionar el micro dentro de los devices
         //https://www.youtube.com/watch?v=HqZrDRwGkdI
         static void Main(string[] args)
         {
-            initvoiceDetection();
-            MMDeviceCollection devices = en.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
+            AudioTracking audio = new AudioTracking();
 
-            getBackgroundNoise(ref devices); //Cogemos el sonido de fondo de donde este el men
-            voiceTest(ref devices);          //Le cogemos el volumen al que habla, para que saber bien cuando grita
+            audio.initvoiceDetection();
+            MMDeviceCollection devices = audio.getDevices();
+
+            audio.getBackgroundNoise(ref devices); //Cogemos el sonido de fondo de donde este el men
+            audio.voiceTest(ref devices);          //Le cogemos el volumen al que habla, para que saber bien cuando grita
 
 
             Console.WriteLine(devices[2].ToString());
 
             while (devices.Count > 0)
             {
-                if ((devices[2].AudioMeterInformation.MasterPeakValue * voiceMult) > defaultspakingVolume)
-                { 
+                if ((devices[2].AudioMeterInformation.MasterPeakValue * audio.getVoiceMult()) > audio.getDefaultSpakingVolume())
+                {
                     Console.WriteLine("Susto");
                 }
 
@@ -49,9 +48,13 @@ namespace AudioTracking
 
         }
 
+        public void initvoiceDetection()
+        {
+            en = new MMDeviceEnumerator();
+        }
         //Método para saber a que volumen habla de normal el usuario, para poder tenerlo como referenica
         //ASí si habla sin más no lo cuenta como grito.
-        static void voiceTest(ref MMDeviceCollection devices)
+        public void voiceTest(ref MMDeviceCollection devices)
         {
             float voice = 0.0f;
             int speakingCont = 0;               //Las veces que ha hablado
@@ -63,7 +66,7 @@ namespace AudioTracking
             while (speakingCont < voiceTests)
             {
                 voice = devices[2].AudioMeterInformation.MasterPeakValue;
-                                                                //Representa que ya esta hablando
+                //Representa que ya esta hablando
                 if ((voice * voiceMult) > backgroundNoise && !speaking)
                 {
                     samples[speakingCont] = voice * voiceMult;
@@ -76,7 +79,7 @@ namespace AudioTracking
             defaultspakingVolume = acumVoice / voiceTests;
         }
 
-        static void getBackgroundNoise(ref MMDeviceCollection devices)
+        public void getBackgroundNoise(ref MMDeviceCollection devices)
         {
             int timeRecording = 0;
             float backgroundAcum = 0.0f;
@@ -90,6 +93,15 @@ namespace AudioTracking
 
             backgroundNoise = backgroundAcum / backgroundTimer;
         }
+
+        public MMDeviceCollection getDevices()
+        {
+            return en.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
+        }
+
+        public float getVoiceMult() { return voiceMult; }
+
+        public float getDefaultSpakingVolume() { return defaultspakingVolume; }
 
     }
 }
