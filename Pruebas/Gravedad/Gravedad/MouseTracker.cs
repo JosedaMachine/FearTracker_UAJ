df368098c2cec8f8e0d5a3c8edbec67b9e4c4bf3
mihern07 <mihern07@ucm.es>
2023-05-09 19:23:15 +0200

    Movido mousetracking a otra clase

--- Pruebas/Gravedad/Gravedad/MouseTracker.cs
+++ Pruebas/Gravedad/Gravedad/MouseTracker.cs
@@ -4,23 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Gravedad
 {
     internal class MouseTracker
     {
 
-        public void initMouseTracker() { 
+        #region MouseVariables
+        Point oldMousePosition;   //Position of the mouse in the previous iteration
+        int mousePositionDifference;    //Difference in position between previous iteration and current one
+        int offsetMouseDifference;  //Offset
+        double averageDifference;  //Average movement, this variable changes during execution and decides if a movement is sporadic or not
+        long numberOfMovements;     //Number of times the average has been determined
+        float scaredMouseMultiplier; //Multiplier applied to the average to determine if the movement of the mouse was sporadic or not
+                                            //Decrease to increase sensitivity
 
-        }
-         void MousePathTracker()
+        DateTime lastMouseScareTime; //Saves when was the last mouse scare to avoid repeating the same input
+        int mouseScareTimeOffset;   //Minimum offset between scares in seconds
+        #endregion
+
+        public MouseTracker()
         {
+            oldMousePosition = Cursor.Position;
+            offsetMouseDifference = 20;
+            averageDifference = 0;
+            numberOfMovements = 0;
+            scaredMouseMultiplier = 1.7f;
 
+            lastMouseScareTime = DateTime.Now;
+            mouseScareTimeOffset = 1;
         }
 
-        void MouseClickTracker()
+        public void readInput(DateTime currentTime)
         {
+            Point currentMousePosition = Cursor.Position;
 
+            //We calculate how much the muse has moved in this iteration
+            int mouseDifferenceX = Math.Abs(oldMousePosition.X - currentMousePosition.X);
+            int mouseDifferenceY = Math.Abs(oldMousePosition.Y - currentMousePosition.Y);
+            mousePositionDifference = mouseDifferenceX + mouseDifferenceY;
+
+            //We only determine the average and register the input if the difference surpasses the offset
+            if (mousePositionDifference >= offsetMouseDifference)
+            {
+                //We avoid getting various mouseScareEvents if one has already ocurred recently
+                if ((currentTime - lastMouseScareTime).Seconds > mouseScareTimeOffset)
+                {
+                    //Determining if the user was scared or not
+                    if (mousePositionDifference > averageDifference * scaredMouseMultiplier)
+                    {
+                        lastMouseScareTime = DateTime.Now;
+                        Console.WriteLine("SUSTO RATON");
+                    }
+                }
+
+                //We determine the new average
+                numberOfMovements++;
+                averageDifference = (double)((averageDifference * (numberOfMovements - 1) + mousePositionDifference)) / (double)numberOfMovements;
+                oldMousePosition = currentMousePosition;
+                //Console.WriteLine("Diferencia de movimiento: X={0}", mousePositionDifference);
+            }
         }
 
 
--- Pruebas/Gravedad/Gravedad/Program.cs
+++ Pruebas/Gravedad/Gravedad/Program.cs
@@ -45,23 +45,13 @@ namespace Gravedad
 
             //Trackers
             InputTracker inputTracker = new InputTracker();
+            MouseTracker mouseTracker = new MouseTracker();
 
             //formsEntry.Start();
 
             DateTime currentTime;
 
-            #region MouseVariables
-            Point oldMousePosition = Cursor.Position;   //Position of the mouse in the previous iteration
-            int mousePositionDifference;    //Difference in position between previous iteration and current one
-            int offsetMouseDifference = 20;  //Offset
-            double averageDifference = 0;  //Average movement, this variable changes during execution and decides if a movement is sporadic or not
-            long numberOfMovements = 0;     //Number of times the average has been determined
-            float scaredMouseMultiplier = 1.7f; //Multiplier applied to the average to determine if the movement of the mouse was sporadic or not
-                                                //Decrease to increase sensitivity
-
-            DateTime lastMouseScareTime = DateTime.Now; //Saves when was the last mouse scare to avoid repeating the same input
-            int mouseScareTimeOffset = 1;   //Minimum offset between scares in seconds
-            #endregion
+            
 
             while (!processToTrack.HasExited)
             {
@@ -69,36 +59,7 @@ namespace Gravedad
 
                 if (trackerParams.mouseTracking)
                 {
-                    #region MouseTracking
-                    Point currentMousePosition = Cursor.Position;
-
-                    //We calculate how much the muse has moved in this iteration
-                    int mouseDifferenceX = Math.Abs(oldMousePosition.X - currentMousePosition.X);
-                    int mouseDifferenceY = Math.Abs(oldMousePosition.Y - currentMousePosition.Y);
-                    mousePositionDifference = mouseDifferenceX + mouseDifferenceY;
-
-                    //We only determine the average and register the input if the difference surpasses the offset
-                    if (mousePositionDifference >= offsetMouseDifference)
-                    {
-                        //We avoid getting various mouseScareEvents if one has already ocurred recently
-                        if ((currentTime - lastMouseScareTime).Seconds > mouseScareTimeOffset)
-                        {
-                            //Determining if the user was scared or not
-                            if (mousePositionDifference > averageDifference * scaredMouseMultiplier)
-                            {
-                                lastMouseScareTime = DateTime.Now;
-                                Console.WriteLine("SUSTO RATON");
-                            }
-                        }
-
-                        //We determine the new average
-                        numberOfMovements++;
-                        averageDifference = (double)((averageDifference * (numberOfMovements - 1) + mousePositionDifference)) / (double)numberOfMovements;
-                        oldMousePosition = currentMousePosition;
-                        //Console.WriteLine("Diferencia de movimiento: X={0}", mousePositionDifference);
-
-                    }
-                    #endregion
+                    mouseTracker.readInput(currentTime);
                 }
 
                 inputTracker.readInput();
